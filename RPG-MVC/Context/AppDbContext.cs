e97d3c17ef41c346a0681f8965f3f6b8a6e77036
Hugo Victor <hugovic.it@gmail.com>
2023-06-09 04:59:22 -0300

    Configuracao do DbContext

---
+++ RPG-MVC/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RPG_MVC.Context
+{
+    public class AppDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=banco.db;Cache=Shared");
+        }
+    }
+}
