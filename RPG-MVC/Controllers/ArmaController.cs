1574973e14d2bf570abbc54b622c009bef243941
Hugo Victor <hugovic.it@gmail.com>
2023-06-11 09:19:28 -0300

    Inicializacao do ArmaController

--- RPG-MVC/Controllers/ArmaController.cs
+++ RPG-MVC/Controllers/ArmaController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using RPG_MVC.Context;
+using RPG_MVC.Models;
 
 namespace RPG_MVC.Controllers
 {
@@ -16,5 +17,42 @@ namespace RPG_MVC.Controllers
         {
             return View();
         }
+
+        public ActionResult AdicionarArma()
+        {
+            return View();
+        }
+        public ActionResult AdicionandoArma(Arma arma)
+        {
+            if (arma == null) {
+                return View("Index");
+            }
+            try{
+                _context.Armas.Add(arma);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+            }
+            return View(arma);
+        }
+
+        public ActionResult VisualizarTodasArmas()
+        {
+            List<Arma> armas;
+            try
+            {
+                armas = _context.Armas.ToList();
+            }
+            catch (Exception e)
+            {
+                return View("Index");
+            }
+            return View(armas);
+        }
+
+
+
+        
     }
 }
---
+++ RPG-MVC/Migrations/20230611121046_ArmasModel.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RPG_MVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArmasModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Armas",
+                columns: table => new
+                {
+                    ArmaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nome = table.Column<string>(type: "TEXT", maxLength: 80, nullable: true),
+                    Descricao = table.Column<string>(type: "TEXT", maxLength: 300, nullable: true),
+                    Ataque = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Armas", x => x.ArmaId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Armas");
+        }
+    }
+}
